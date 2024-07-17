bc5c02b47169bc43c0f1c5ce02ca80002dcf5c47
anchel <anchel.xy@gmail.com>
2024-07-17 23:35:05 +0800

    feat: update

--- controllers/login-controller.go
+++ controllers/login-controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/anchel/sell-copilot-server/routes"
 	"github.com/gin-gonic/gin"
 )
@@ -22,7 +24,19 @@ type LoginController struct {
 	*BaseController
 }
 
+type loginForm struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func (c *LoginController) Login(ctx *gin.Context) {
+	var form loginForm
+	if err := ctx.ShouldBind(&form); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "invalid form",
+		})
+		return
+	}
 	ctx.JSON(200, gin.H{
 		"message": "login",
 	})
